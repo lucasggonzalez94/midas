fd5a6e84924a0713f2dd1ad346102dd6f5392ed3
Lucas Gonzalez <lucasggonzalez94@outlook.com>
2021-03-22 19:52:03 -0300

    Cierre de sesion despues de 5 minutos de inactividad

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/Login';
@@ -15,11 +15,41 @@ function App() {
     }
 
     guardarLocalStorage(usuariosRegistrados, login);
-	
-	// Sesion cerrada despues de 5 minutos
-	setTimeout(() => {
-		setLogin(false);
-	}, 300000);
+
+	useEffect(() => {
+		// Cerrar sesion despues de 5 minutos de inactividad
+		let timeoutID;
+
+		function setup() {
+			window.addEventListener("mousemove", resetTimer, false);
+			window.addEventListener("mousedown", resetTimer, false);
+			window.addEventListener("keypress", resetTimer, false);
+			window.addEventListener("DOMMouseScroll", resetTimer, false);
+			window.addEventListener("mousewheel", resetTimer, false);
+			window.addEventListener("touchmove", resetTimer, false);
+			window.addEventListener("MSPointerMove", resetTimer, false);
+
+			startTimer();
+		}
+		setup();
+
+		function startTimer() {
+			timeoutID = window.setTimeout(goInactive, 300000);
+		}
+
+		function resetTimer(e) {
+			window.clearTimeout(timeoutID);
+			goActive();
+		}
+
+		function goInactive() {
+			setLogin(false);
+		}
+
+		function goActive() {
+			startTimer();
+		}
+	}, [])
 
   	return (
     	<Router>
